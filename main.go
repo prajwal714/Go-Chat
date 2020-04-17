1c90449044fd57d34584ed0ee9ed4f20abb7ccd8
prajwal714 <prajwal714singh@gmail.com>
2020-04-17 21:10:43 +0530

    added Port flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -19,10 +20,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() //parse the flags
+
 	r := newRoom()
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -31,7 +36,8 @@ func main() {
 	go r.run()
 
 	//start the web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting the web server at: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("Listen And serve: ", err)
 	}
 }
