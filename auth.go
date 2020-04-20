5d55c937705075bf964a0547744f5cfa3d4c8575
prajwal714 <prajwal714singh@gmail.com>
2020-04-20 13:30:55 +0530

    added comments

--- auth.go
+++ auth.go
@@ -2,27 +2,29 @@ package main
 
 import "net/http"
 
-type authHandler struct{
+type authHandler struct {
 	next http.Handler
 }
 
-func (h *authHandler) ServeHTTP( w http.ResponseWriter, r *http.Request){
-	_,err:=r.Cookie("auth")
-	if err==http.ErrNoCookie {
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// _ is used to collect cookie, we discardd it coz we are only interested if it is presnt or not.
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
 		//not authenticated
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
 		return
 	}
 
-	if err !=nil {
+	if err != nil {
 		//some other error
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	//success-- call the next handler
-	h.next.ServeHTTP(w,r)
+	h.next.ServeHTTP(w, r)
 }
 
 func MustAuth(handler http.Handler) http.Handler {
